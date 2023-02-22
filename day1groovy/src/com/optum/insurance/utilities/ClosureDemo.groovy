52636d2c96d84b942673f204ae51d1915190df66
eswaribala <parameswaribala@gmail.com>
2023-02-22 10:58:08 +0530

    updated

--- day1groovy/src/com/optum/insurance/utilities/ClosureDemo.groovy
+++ day1groovy/src/com/optum/insurance/utilities/ClosureDemo.groovy
@@ -7,18 +7,23 @@ import com.optum.insurance.models.PolicyHolder
 import java.time.LocalDate
 
 
-def policyHolders=[]
-def addressList=[]
+List<PolicyHolder> policyHolders=new ArrayList<PolicyHolder>()
+List<Address> addressList=new ArrayList<Address>();
 
 
-for(def i : 1..100){
+for(def i : 1..10){
 
-    policyHolders.add(new PolicyHolder(new Random().nextInt(1000000), LocalDate.of(2015,new Random().nextInt(10)+1,
+    policyHolders.add(new PolicyHolder(new Random().nextInt(10000000), LocalDate.of(2015,new Random().nextInt(10)+1,
             new Random().nextInt(26)+1), LocalDate.of(2020,new Random().nextInt(10)+1,
             new Random().nextInt(26)+1),
             "PH"+new Random().nextInt(1),
-            new Random().nextInt(1000000), Gender.FEMALE, addressList.add(new Address("8d","street","Chennai","TN")),"Param@gmail.com",9952032862,))
+            new Random().nextInt(1000000), Gender.FEMALE,
+            null,
+            "Param@gmail.com",9952032862L,))
 
 }
 
-
+println "${policyHolders.size()}"
+for(policyHolder in policyHolders){
+    println "${policyHolder}"
+}
