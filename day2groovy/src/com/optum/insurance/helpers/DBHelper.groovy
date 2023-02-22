76e46730bbdf0f5d887e4f25df0108e87e26657a
eswaribala <parameswaribala@gmail.com>
2023-02-22 17:36:19 +0530

    updated

---
+++ day2groovy/src/com/optum/insurance/helpers/DBHelper.groovy
@@ -0,0 +1,26 @@
+package com.optum.insurance.helpers
+
+import groovy.sql.Sql
+
+class DBHelper {
+
+
+  def static Sql getConnection(){
+
+    Properties properties=new Properties()
+    File propertyFile=new File(getPropertyFileName())
+    propertyFile.withInputStream {properties.load(it)}
+
+    //db connection
+    return Sql.newInstance(properties.url, properties.userName,properties.password, properties.driverClassName)
+
+  }
+
+
+
+  def static String getPropertyFileName(){
+
+     return "./src/com/optum/insurance/resources/db.properties"
+  }
+
+}
