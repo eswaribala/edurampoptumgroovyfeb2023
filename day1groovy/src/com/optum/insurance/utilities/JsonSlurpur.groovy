f65c113c78417aa6ab30695ba0392f0d0f5db3dc
eswaribala <parameswaribala@gmail.com>
2023-02-22 15:54:59 +0530

    updated

--- day1groovy/src/com/optum/insurance/utilities/JsonSlurpur.groovy
+++ day1groovy/src/com/optum/insurance/utilities/JsonSlurpur.groovy
@@ -1,14 +1,19 @@
 package com.optum.insurance.utilities
 
+import com.optum.insurance.models.Product
 import groovy.json.JsonSlurper
 
 
 def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText('{ "name": "Laptop" } /* some comment */')
-
+def object = jsonSlurper.parseText('{ "name": "Laptop" , "price":4385834} /* some comment */')
+println "$object"
+def product=new Product(object.name,object.price)
 assert object instanceof Map
 assert object.name == 'Laptop'
 
 
 
-
+//check file exists
+File file = new File("users.txt")
+def lines = file.readLines()
+println "$lines"
