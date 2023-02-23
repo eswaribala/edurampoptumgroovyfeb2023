83ddde6e7e5c5fc976858903b681468203c86dec
eswaribala <parameswaribala@gmail.com>
2023-02-23 12:08:50 +0530

    updated

--- day1gradle/src/main/groovy/com/optum/insurance/utilities/XMLReaderDemo.groovy
+++ day1gradle/src/main/groovy/com/optum/insurance/utilities/XMLReaderDemo.groovy
@@ -1,8 +1,15 @@
 package com.optum.insurance.utilities
 
 import groovy.xml.XmlParser
-
-def productsData=new XmlParser().parse(new File("F:\\groovyoptumfeb2023\\day1gradle\\src\\main\\resources\\product.xml"))
+import groovy.xml.XmlSlurper
+File file=new File("F:\\groovyoptumfeb2023\\day1gradle\\src\\main\\resources\\product.xml")
+def productsData=new XmlParser().parse(file)
 productsData.Product.each {
     println "Product Name= ${it.Name}"
+}
+
+def xml = new XmlSlurper().parse(file)
+
+xml.each {
+    println "${it}"
 }
