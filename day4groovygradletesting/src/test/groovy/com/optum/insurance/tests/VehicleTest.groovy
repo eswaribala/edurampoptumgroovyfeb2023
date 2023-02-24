2e4e43d41b8436d1dcbcefc9f32601ed4fedc761
eswaribala <parameswaribala@gmail.com>
2023-02-24 11:21:24 +0530

    updated

--- day4groovygradletesting/src/test/groovy/com/optum/insurance/tests/VehicleTest.groovy
+++ day4groovygradletesting/src/test/groovy/com/optum/insurance/tests/VehicleTest.groovy
@@ -3,7 +3,13 @@ package com.optum.insurance.tests
 import com.optum.insurance.Vehicle
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
+import java.time.LocalDate
+
+import static org.junit.jupiter.api.Assertions.assertFalse
+import static org.junit.jupiter.api.Assertions.assertNotEquals
 import static org.junit.jupiter.api.Assertions.assertNotNull
 
 class VehicleTest {
@@ -17,6 +23,14 @@ class VehicleTest {
     @Test
     void vehicleInstanceExists(){
         assertNotNull(vehicle)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [ "2017-12-27", "2000-11-12", "2020-11-04","2022-01-15","1995-03-27","1975-08-12","2002-04-19" ])
+    void vehicleExpiryTest(String dor) {
+
+        vehicle.setDateOfRegistration(LocalDate.parse(dor))
+       assertFalse((LocalDate.now().getYear()-vehicle.dateOfRegistration.getYear())>15)
     }
 
 }
